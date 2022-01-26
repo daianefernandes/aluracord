2f35591f614c947c1b2f054bf476e91cdd661769
daianefernandes <daianedsdaia@gmail.com>
2022-01-26 07:37:07 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button } from '@skynexui/components';
+
+function HomePage() {
+    return (
+        <>
+            <div>
+                Welcome to Next.js!
+            </div>
+            <Button>Sair</Button>
+        </>
+    )
+  }
+  
+  export default HomePage
